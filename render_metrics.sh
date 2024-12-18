5c169c67a7e9d6eb6460fc7eccc1392be5fb5b2f
ZhangAoCanada <zhangaocanada@gmail.com>
2024-12-18 13:33:40 +0800

    adapted render scripts

--- render_metrics.sh
+++ render_metrics.sh
@@ -1,6 +1,8 @@
-output_folder=$1
+test_path=data/bdaibdai___MatrixCity/small_city/blockA_fusion/test
+output_folder=outputs/pretrained/matrixcity
+gpu_list=3
 iteration=60000
 
-python render.py -m ${output_folder} --iteration ${iteration} --skip_train
-python metrics.py -m ${output_folder}
+CUDA_VISIBLE_DEVICES=${gpu_list} python render.py -m ${output_folder} --iteration ${iteration} --skip_train --custom_test ${test_path} --resolution 1
+# CUDA_VISIBLE_DEVICES=${gpu_list} python metrics.py -m ${output_folder}
 
